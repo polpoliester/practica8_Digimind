edfdf55874bb9d819bcf6c7e77929aadb56594d7
polpoliester <pol_poliester@hotmail.com>
2025-06-17 15:46:21 -0700

    Proyecto creado

---
+++ MyDigimind_VazquezPaul/app/src/main/java/vazquez/paul/mydigimind_vazquezpaul/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package vazquez.paul.mydigimind_vazquezpaul.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ MyDigimind_VazquezPaul/app/src/main/java/vazquez/paul/mydigimind_vazquezpaul/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package vazquez.paul.mydigimind_vazquezpaul.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
