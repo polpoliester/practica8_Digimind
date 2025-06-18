726c9e1e3159539032e3a69c8621ceed8eb1d94f
polpoliester <pol_poliester@hotmail.com>
2025-06-18 15:20:44 -0700

    Revert "finished"
    This reverts commit ee83c277642bcb6610fba3f4ab054d82e6220d89.

--- MyDigimind_VazquezPaul/app/src/main/java/vazquez/paul/mydigimind_vazquezpaul/ui/Task.kt
+++
@@ -1,5 +0,0 @@
-package vazquez.paul.mydigimind_vazquezpaul.ui
-
-data class Task (var title: String,
-                 var days: ArrayList<String>,
-                 var time: String)
--- MyDigimind_VazquezPaul/app/src/main/java/vazquez/paul/mydigimind_vazquezpaul/ui/home/HomeFragment.kt
+++ MyDigimind_VazquezPaul/app/src/main/java/vazquez/paul/mydigimind_vazquezpaul/ui/home/HomeFragment.kt
@@ -1,26 +1,17 @@
 package vazquez.paul.mydigimind_vazquezpaul.ui.home
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.GridView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import vazquez.paul.mydigimind_vazquezpaul.R
-import vazquez.paul.mydigimind_vazquezpaul.ui.Task
 
 class HomeFragment : Fragment() {
-    private var adaptador: AdaptadorTareas? = null
-    private lateinit var homeViewModel : HomeViewModel
 
-    companion object {
-        var tasks = ArrayList<Task>()
-        var first = true
-    }
+    private lateinit var homeViewModel : HomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,68 +21,9 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
 
-        if (first) {
-            fillTasks()
-            first = false
-        }
-
-        adaptador = AdaptadorTareas(root.context, tasks)
-
-        val gridView: GridView = root.findViewById(R.id.gridView)
-
-        gridView.adapter = adaptador
-
+        })
         return root
     }
-    fun fillTasks(){
-        tasks.add(Task("Practice 1", arrayListOf("Tuesday"), "17:30"))
-        tasks.add(Task("Practice 2", arrayListOf("Monday", "Sunday"), "17:40"))
-        tasks.add(Task("Practice 3", arrayListOf("Wednesday"), "14:00"))
-        tasks.add(Task("Practice 4", arrayListOf("Saturday"), "11:00"))
-        tasks.add(Task("Practice 5", arrayListOf("Friday"), "13:00"))
-        tasks.add(Task("Practice 6", arrayListOf("Thursday"), "10:40"))
-        tasks.add(Task("Practice 7", arrayListOf("Monday"), "12:30"))
-    }
-
-    private class AdaptadorTareas: BaseAdapter {
-        var tasks = ArrayList<Task>()
-        var contexto: Context? = null
-
-        constructor(contexto: Context, tasks: ArrayList<Task>){
-            this.contexto = contexto
-            this.tasks = tasks
-        }
-
-        override fun getCount(): Int {
-            return tasks.size
-        }
-
-        override fun getItem(position: Int): Any {
-            return tasks[position]
-        }
-
-        override fun getItemId(position: Int): Long {
-            return position.toLong()
-        }
-
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-            var task = tasks[position]
-            var inflator = LayoutInflater.from(contexto)
-            var vista = inflator.inflate(R.layout.task_view, null)
-
-            var tv_title: TextView = vista.findViewById(R.id.tv_title)
-            var tv_time: TextView = vista.findViewById(R.id.tv_time)
-            var tv_days: TextView = vista.findViewById(R.id.tv_days)
-
-            tv_title.setText(task.title)
-            tv_time.setText(task.time)
-            tv_days.setText(task.days.toString())
-
-            return vista
-        }
-
-
-    }
-
 }
