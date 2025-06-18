ee83c277642bcb6610fba3f4ab054d82e6220d89
polpoliester <pol_poliester@hotmail.com>
2025-06-18 15:09:34 -0700

    finished

---
+++ MyDigimind_VazquezPaul/app/src/main/java/vazquez/paul/mydigimind_vazquezpaul/ui/Task.kt
@@ -0,0 +1,5 @@
+package vazquez.paul.mydigimind_vazquezpaul.ui
+
+data class Task (var title: String,
+                 var days: ArrayList<String>,
+                 var time: String)
--- MyDigimind_VazquezPaul/app/src/main/java/vazquez/paul/mydigimind_vazquezpaul/ui/dashboard/DashboardFragment.kt
+++ MyDigimind_VazquezPaul/app/src/main/java/vazquez/paul/mydigimind_vazquezpaul/ui/dashboard/DashboardFragment.kt
@@ -1,15 +1,21 @@
 package vazquez.paul.mydigimind_vazquezpaul.ui.dashboard
 
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import vazquez.paul.mydigimind_vazquezpaul.R
-import vazquez.paul.mydigimind_vazquezpaul.databinding.FragmentDashboardBinding
+import vazquez.paul.mydigimind_vazquezpaul.ui.Task
+import vazquez.paul.mydigimind_vazquezpaul.ui.home.HomeFragment
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 class DashboardFragment : Fragment() {
 
@@ -23,9 +29,65 @@ class DashboardFragment : Fragment() {
         val dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
 
-        })
+        val btn_time: Button = root.findViewById(R.id.btn_time)
+
+        btn_time.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener{timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+
+                btn_time.text = SimpleDateFormat("HH:mm").format(cal.time)
+
+            }
+            TimePickerDialog(root.context, timeSetListener, cal.get(Calendar.HOUR_OF_DAY),
+                cal.get(Calendar.MINUTE), true).show()
+
+        }
+        val btn_save = root.findViewById<Button>(R.id.btn_save)
+        val et_titulo = root.findViewById<EditText>(R.id.et_task)
+        val checkMonday = root.findViewById<CheckBox>(R.id.checkMonday)
+        val checkTuesday = root.findViewById<CheckBox>(R.id.checkTuesday)
+        val checkWednesday = root.findViewById<CheckBox>(R.id.checkWednesday)
+        val checkThursday = root.findViewById<CheckBox>(R.id.checkThursday)
+        val checkFriday = root.findViewById<CheckBox>(R.id.checkFriday)
+        val checkSaturday = root.findViewById<CheckBox>(R.id.checkSaturday)
+        val checkSunday = root.findViewById<CheckBox>(R.id.checkSunday)
+
+        btn_save.setOnClickListener {
+            var title = et_titulo.text.toString()
+            var time = btn_time.text.toString()
+            var days = ArrayList<String>()
+
+            if (checkMonday.isChecked) {
+                days.add("Monday")
+            }
+            if (checkTuesday.isChecked) {
+                days.add("Tuesday")
+            }
+            if (checkWednesday.isChecked) {
+                days.add("Wednesday")
+            }
+            if (checkThursday.isChecked) {
+                days.add("Thursday")
+            }
+            if (checkFriday.isChecked) {
+                days.add("Friday")
+            }
+            if (checkSaturday.isChecked) {
+                days.add("Saturday")
+            }
+            if (checkSunday.isChecked) {
+                days.add("Sunday")
+            }
+
+            var task = Task(title, days, time)
+
+            HomeFragment.tasks.add(task)
+
+            Toast.makeText(root.context, "new task added!", Toast.LENGTH_SHORT).show()
+        }
         return root
     }
 }
